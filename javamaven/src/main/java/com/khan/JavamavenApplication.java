cd35fa1fb3029f56d6a6066d8e2719983d38ecd1
Akhlaque <akhlaquekhan_mca@rediffmail.com>
2021-02-10 19:54:09 +0530

    maven commit

---
+++ javamaven/src/main/java/com/khan/JavamavenApplication.java
@@ -0,0 +1,13 @@
+package com.khan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamavenApplication.class, args);
+	}
+
+}
